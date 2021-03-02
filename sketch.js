27a3134813e8edb73112cb6d46fcb98dc122084f
AnshulGanediwal <ganediwalmitali1@gmail.com>
2021-03-02 11:15:11 +0530

    first commit

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+var box1,box2,box3;
 
-var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -8,24 +9,25 @@ const Body = Matter.Body;
 
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+	helicopterIMG=loadImage("helicopter.png");
+	packageIMG=loadImage("package.png");
 }
 
 function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
 	
+
 	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale=0.2;
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale=0.6;
 
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
+	groundSprite=createSprite(width/2,690, width,10);
+	groundSprite.shapeColor=color(255);
 
 
 	engine = Engine.create();
@@ -36,30 +38,60 @@ function setup() {
 	
 
 	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
+	ground = Bodies.rectangle(width/2, 690, width, 20 , {isStatic:true} );
+	World.add(world, ground);
+	 
+
+	box1=new box(275,635,20,100);
+	box2=new box(385,675,200,20);
+	box3=new box(480,635,20,100);
+
+
 
 	Engine.run(engine);
-	 
+  
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  packageSprite.x= packageBody.position.x ;
+  packageSprite.y= packageBody.position.y ;
+  
+
+  //To draw the sprites
   drawSprites();
+
+  //To display the all boxes bodies
+  box1.display();
+  box2.display();
+  box3.display();
  
 }
 
 function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-	Matter.Body.setStatic(packageBody,false);
-     
-  }
+		if (keyCode === DOWN_ARROW) {
+			// Look at the hints in the document and understand how to make the package body fall only on
+			Matter.Body.setStatic(packageBody,false);
+		}
+
+		if(keyCode===LEFT_ARROW){
+            helicopterSprite.x=helicopterSprite.x-20; 
+			translation={x:-20,y:0}; 
+			Matter.Body.translate(packageBody, translation);
+		}
+
+		if(keyCode===RIGHT_ARROW){
+			helicopterSprite.x=helicopterSprite.x+20;
+			translation={x:+20,y:0}; 
+			Matter.Body.translate(packageBody, translation) 
+		}
+
+		
+
 }
+
 
 
 
